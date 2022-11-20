9067733a2f4d5d6160ab8ec51cbeda1a01a9b5c9
DogukanSakin <86911611+DogukanSakin@users.noreply.github.com>
2022-11-20 23:08:48 +0300

    UI improvements for main and detail activities.

--- app/src/main/java/com/example/artbookify/DetailsActivity.kt
+++ app/src/main/java/com/example/artbookify/DetailsActivity.kt
@@ -2,16 +2,21 @@ package com.example.artbookify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.artbookify.databinding.ActivityDetailsBinding
-import com.example.artbookify.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding;
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater);
+        binding = ActivityDetailsBinding.inflate(layoutInflater);
         val view = binding.root;
         setContentView(view);
+    }
+    fun saveInfo(view: View){
+
+    }
+    fun uploadImage(view:View){
 
     }
 }
